4e46da0d879ce23c7ef8142599131d5a440d75ac
Bharti20 <bhartikumari20@navgurukul.org>
2020-12-10 00:57:22 +0800

    solve some logical questions

--- first_que_print_content.py
+++ first_que_print_content.py
@@ -1,4 +1,10 @@
 my_file3 = open("people1-exercise.txt","r")
 my_file= my_file3.read()
-print (my_file)
+new_file= my_file.split("\n")
+i=0
+count=0
+while i<len(new_file):
+    count=count+1
+    i=i+1
+print("line", count)
 my_file3.close()
